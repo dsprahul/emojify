18ed659737f867c97dcdd40fe254437cb1e833b2
RahulDamineni <rahul.damineni@cloudsek.com>
2017-10-28 00:45:16 +0530

    Model training complete, accuracy of 50% at best. (Over the 5 categories)

--- building_and_training.py
+++ building_and_training.py
@@ -2,10 +2,14 @@ import os
 import cv2
 import keras
 import numpy as np
+from make_dataset import preprocess_image
 from face_features import FaceFeatures
+from keras import optimizers
 from keras.layers import Dense, Flatten, Input
 from keras.layers import Conv2D, MaxPooling2D
 from keras.models import Model
+from keras.utils import to_categorical
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 
 BATCH_SIZE = 32
 
@@ -38,43 +42,75 @@ def convolution_stack(input_layer):
 
     flattened = Flatten()(c4)
 
-    fc1 = Dense(output_dim=256, activation="relu")(flattened)
-    partial_model_output_layer = Dense(output_dim=64, activation="relu")(fc1)
+    fc1 = Dense(units=256, activation="relu")(flattened)
+    partial_model_output_layer = Dense(units=64, activation="relu")(fc1)
 
     return partial_model_output_layer
 
 
 def build_hierarchial_model():
 
-    eyes_input = Input(shape=(400, 16, 1), name="eyes_input")
+    eyes_input = Input(shape=(400, 80, 1), name="eyes_input")
     eyes_model = convolution_stack(input_layer=eyes_input)
 
-    nose_input = Input(shape=(400, 16, 1), name="nose_input")
+    nose_input = Input(shape=(160, 240, 1), name="nose_input")
     nose_model = convolution_stack(input_layer=nose_input)
 
-    mouth_input = Input(shape=(400, 16, 1), name="mouth_input")
+    mouth_input = Input(shape=(320, 240, 1), name="mouth_input")
     mouth_model = convolution_stack(input_layer=mouth_input)
 
     layer_summation = keras.layers.concatenate([eyes_model,
                                                 nose_model,
                                                 mouth_model])
 
-    fully_connected = Dense(output_dim=32, activation="relu",
+    fully_connected = Dense(units=32, activation="relu",
                             name="fc1")(layer_summation)
-    output_layer = Dense(output_dim=5, activation="softmax",
+    output_layer = Dense(units=5, activation="softmax",
                          name="fc2")(fully_connected)
 
     model = Model(inputs=[eyes_input, nose_input, mouth_input],
-                  output=output_layer)
+                  outputs=[output_layer],
+                  name="prime_model")
 
     # print model.summary()
     return model
 
 
+def load_training_data():
+
+    categories = ["anger", "happiness", "neutral", "sadness", "surprise"]
+    tr_data_dir = "training_data"
+
+    exs, nxs, mxs, ys = [], [], [], []
+    for cid, catg in enumerate(categories):
+
+        for file_ in os.listdir(os.path.join(tr_data_dir, catg)):
+            file_path = os.path.join(tr_data_dir, catg, file_)
+            fs = FaceFeatures(cv2.imread(file_path)).build_face_structures()[0]
+            eyes, nose, mouth = preprocess_image(face_struct=fs)
+
+            exs.append(eyes.reshape(400, 80, 1))
+            nxs.append(nose.reshape(160, 240, 1))
+            mxs.append(mouth.reshape(320, 240, 1))
+            ys.append(cid)
+
+    return np.array(exs), np.array(nxs), np.array(mxs), ys
+
+
 if __name__ == "__main__":
 
-    # model = build_hierarchial_model()
-    # model.compile(optimizer="adam", loss="categorical_crossentropy")
+    model = build_hierarchial_model()
+    model.compile(optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+                  loss="categorical_crossentropy",
+                  metrics=["accuracy"])
 
-    xs, ys = load_training_data()
-    # model.fit(x=xs, y=ys, nb_epoch=1, batch_size=16)
+    exs, nxs, mxs, ys = load_training_data()
+    model.fit(x=[exs, nxs, mxs],
+              y=to_categorical(ys, 5),
+              epochs=50,
+              batch_size=16,
+              validation_split=0.15,
+              callbacks=[
+        ModelCheckpoint('emotion_detect.model',
+                        monitor='val_acc',
+                        save_best_only=True)])
